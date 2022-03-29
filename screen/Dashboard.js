7674a04382e035c5408e9d4521ebf4ef1a1cdc19
mazatlumm <multazamsttm@gmail.com>
2022-03-30 06:05:07 +0700

    Dashboard Finish

--- screen/Dashboard.js
+++ screen/Dashboard.js
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions, ScrollView
 import React from 'react'
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
-import IconBar from '../assets/images/TopBarIcon.png'
+import TopBarIcon2 from '../assets/images/TopBarIcon2.png'
 import MenuIcon from '../assets/images/menuicon.png'
 import Farmer from '../assets/images/farmer.png'
 import iconsetcanal from '../assets/images/iconsetcanal.png'
@@ -31,100 +31,101 @@ const Dashboard = ({navigation}) => {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', height:windowHeight}}>
-      {/* Top Bar */}
-        <View style={styles.TopBarBox}>
-          <View style={{flex:0.5,}}>
-            <Image source={IconBar} style={{width:35, resizeMode:'contain'}} />
+      <ScrollView style={{marginBottom:50}}>
+          {/* Top Bar */}
+          <View style={styles.TopBarBox}>
+            <View style={{flex:0.5,}}>
+              <Image source={TopBarIcon2} style={{width:35, resizeMode:'contain'}} />
+            </View>
+            <View style={{flex:2, justifyContent:'flex-start'}}>
+              <Text style={{ fontFamily: 'Philosopher-Bold', fontSize: 25, marginLeft:10 }}>Kelas Bertani</Text>
+            </View>
+            <TouchableOpacity style={{flex:0.5,}}>
+              <Image source={MenuIcon} style={{width:35, resizeMode:'contain'}} />
+            </TouchableOpacity>
           </View>
-          <View style={{flex:2, justifyContent:'flex-start'}}>
-            <Text style={{ fontFamily: 'Philosopher-Bold', fontSize: 25, marginLeft:10 }}>Kelas Bertani</Text>
-          </View>
-          <TouchableOpacity style={{flex:0.5,}}>
-            <Image source={MenuIcon} style={{width:35, resizeMode:'contain'}} />
-          </TouchableOpacity>
-        </View>
 
-      <ScrollView>
-        {/* Greating Text Box Hijau */}
-        <View style={{width:'100%', paddingHorizontal:20}}>
-          <View style={styles.BoxGreating}>
-            <Image source={Farmer} style={styles.FarmerImg} />
-            <View style={{width:200, marginLeft:15, marginTop:45}}>
-              <Text style={styles.TextGreating}>Teknologi AI dan IoT Pertanian</Text>
-            </View>
-            <View style={{width:180, marginLeft:15, marginTop:20}}>
-              <Text style={styles.TextGreatingExpln}>Monitoring & Control 100% Online 24/7</Text>
+      
+          {/* Greating Text Box Hijau */}
+          <View style={{width:'100%', paddingHorizontal:20}}>
+            <View style={styles.BoxGreating}>
+              <Image source={Farmer} style={styles.FarmerImg} />
+              <View style={{width:200, marginLeft:15, marginTop:45}}>
+                <Text style={styles.TextGreating}>Teknologi AI dan IoT Pertanian</Text>
+              </View>
+              <View style={{width:180, marginLeft:15, marginTop:20}}>
+                <Text style={styles.TextGreatingExpln}>Monitoring & Control 100% Online 24/7</Text>
+              </View>
             </View>
           </View>
-        </View>
-        
-        {/* Scan Tanaman */}
-        <View style={{flexDirection:'row', width:'100%', paddingHorizontal:20, marginTop:20}}>
-          <TouchableOpacity style={styles.BoxScanTanaman}>
-            <Text style={styles.TextScan}>Scan Tanaman</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.BoxSetKanal}>
-            <Image source={iconsetcanal} style={{width:20, height:18, resizeMode:'contain'}} />
-          </TouchableOpacity>
-        </View>
+          
+          {/* Scan Tanaman */}
+          <View style={{flexDirection:'row', width:'100%', paddingHorizontal:20, marginTop:20}}>
+            <TouchableOpacity style={styles.BoxScanTanaman}>
+              <Text style={styles.TextScan}>Scan Tanaman</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.BoxSetKanal}>
+              <Image source={iconsetcanal} style={{width:20, height:18, resizeMode:'contain'}} />
+            </TouchableOpacity>
+          </View>
 
-        <View style={{width:'100%', marginTop:20, alignItems:'flex-start', paddingHorizontal:23}}>
-          <Text style={{fontFamily:'Poppins-Bold', fontSize:14, color:'#0D986A'}}>Automatic & Control Pengairan</Text>
-          <View style={{borderBottomWidth:2, borderBottomColor:'#0D986A', width:40, marginTop:5}}></View>
-        </View>
+          <View style={{width:'100%', marginTop:20, alignItems:'flex-start', paddingHorizontal:23}}>
+            <Text style={{fontFamily:'Poppins-Bold', fontSize:14, color:'#0D986A'}}>Automatic & Control Pengairan</Text>
+            <View style={{borderBottomWidth:2, borderBottomColor:'#0D986A', width:40, marginTop:5}}></View>
+          </View>
 
-        <TouchableOpacity style={{width:'100%', paddingHorizontal:20, marginTop:10, alignItems:'center', justifyContent:'center', position:'relative'}}>
-            <View style={styles.JajarGenjang}>
-            </View>
-            <Text style={{fontFamily:'Poppins-Regular', fontSize:16, color:'black', position:'absolute', left:50, top:25}}>6 Sensor Aktif</Text>
-            <Text style={{fontFamily:'Philosopher-Bold', fontSize:30, color:'black', position:'absolute', left:50, top:50}}>Lahan 1</Text>
-            <Text style={{fontFamily:'Philosopher', fontSize:16, color:'black', position:'absolute', left:50, top:85}}>Komoditas Tembakau</Text>
-            <View style={{position:'absolute', right:0, bottom:30}}>
-              <Image source={tanaman} style={{width:200, height:200,resizeMode:'contain'}} />
-            </View>
-            <View style={{position:'absolute', left:50, bottom:50, flexDirection:'row', alignItems:'center'}}>
-              <Text style={{color:'black',fontFamily:'Poppins-Regular'}}>TEMP : </Text>
-              <Text style={{color:'black',fontFamily:'Poppins-Bold', paddingLeft:5, fontSize:20}}>20°C</Text>
-              <Text style={{color:'black',fontFamily:'Poppins-Regular', paddingLeft:10}}>HUM : </Text>
-              <Text style={{color:'black',fontFamily:'Poppins-Bold', paddingLeft:5, fontSize:20}}>50%</Text>
-            </View>
-            <View style={{position:'absolute', left:50, bottom:30}}>
-              <Text style={{fontFamily:'Philosopher', fontSize:14, color:'black'}}>Lokasi : Ds. Grogol, Kec, Diwek, Kab. Jombang</Text>
-            </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={{width:'100%', marginTop:20, paddingHorizontal:20, position:'relative'}}>
-          <View style={{height:160, backgroundColor:'#8CEC8A', opacity:0.3, borderRadius:20}}>
-          </View>
-          <View style={{width:240, position:'absolute', left:40, top:20}}>
-            <Text style={{fontFamily:'Philosopher', fontSize:24, color:'black', position:'absolute'}}>Cek Kesuburan Tanah Lahan Pertanian</Text>
-          </View>
-          <View style={{width:160, position:'absolute', left:40, top:80}}>
-            <Text style={{fontFamily:'Poppins-Bold', fontSize:14, color:'#0D986A', position:'absolute'}}>Hidupkan perangkat IoT dan tancapkan sensor ke tanah</Text>
-          </View>
-          <View style={{width:50,height:50, borderRadius:25, position:'absolute', backgroundColor:'#0D986A', right:40, top:20}}></View>
-          <View style={{width:15,height:15, borderRadius:15/2, position:'absolute', backgroundColor:'#0D986A', left:30, top:5}}></View>
-          <View style={{width:10,height:10, borderRadius:10/2, position:'absolute', backgroundColor:'#0D986A', left:23, top:30}}></View>
-          <View style={{width:30,height:30, borderRadius:30/2, position:'absolute', backgroundColor:'#0D986A', right:23, top:70}}></View>
-          <View style={{width:20,height:20, borderRadius:20/2, position:'absolute', backgroundColor:'#0D986A', left:170, bottom:10}}></View>
-          <TouchableOpacity style={{backgroundColor:'#0D986A', borderRadius:10, paddingHorizontal:20, paddingVertical:5, position:'absolute', bottom:30, right:80}}>
-            <Text style={{fontFamily:'Poppins-Bold', fontSize:18, color:'white'}}>Mulai</Text>
+          <TouchableOpacity style={{width:'100%', paddingHorizontal:20, marginTop:10, alignItems:'center', justifyContent:'center', position:'relative'}}>
+              <View style={styles.JajarGenjang}>
+              </View>
+              <Text style={{fontFamily:'Poppins-Regular', fontSize:16, color:'black', position:'absolute', left:50, top:25}}>6 Sensor Aktif</Text>
+              <Text style={{fontFamily:'Philosopher-Bold', fontSize:30, color:'black', position:'absolute', left:50, top:50}}>Lahan 1</Text>
+              <Text style={{fontFamily:'Philosopher', fontSize:16, color:'black', position:'absolute', left:50, top:85}}>Komoditas Tembakau</Text>
+              <View style={{position:'absolute', right:0, bottom:30}}>
+                <Image source={tanaman} style={{width:200, height:200,resizeMode:'contain'}} />
+              </View>
+              <View style={{position:'absolute', left:50, bottom:50, flexDirection:'row', alignItems:'center'}}>
+                <Text style={{color:'black',fontFamily:'Poppins-Regular'}}>TEMP : </Text>
+                <Text style={{color:'black',fontFamily:'Poppins-Bold', paddingLeft:5, fontSize:20}}>20°C</Text>
+                <Text style={{color:'black',fontFamily:'Poppins-Regular', paddingLeft:10}}>HUM : </Text>
+                <Text style={{color:'black',fontFamily:'Poppins-Bold', paddingLeft:5, fontSize:20}}>50%</Text>
+              </View>
+              <View style={{position:'absolute', left:50, bottom:30}}>
+                <Text style={{fontFamily:'Philosopher', fontSize:14, color:'black'}}>Lokasi : Ds. Grogol, Kec, Diwek, Kab. Jombang</Text>
+              </View>
           </TouchableOpacity>
-        </TouchableOpacity>
+
+          <TouchableOpacity style={{width:'100%', marginTop:20, paddingHorizontal:20, position:'relative'}}>
+            <View style={{height:160, backgroundColor:'#8CEC8A', opacity:0.3, borderRadius:20}}>
+            </View>
+            <View style={{width:240, position:'absolute', left:40, top:20}}>
+              <Text style={{fontFamily:'Philosopher', fontSize:24, color:'black', position:'absolute'}}>Cek Kesuburan Tanah Lahan Pertanian</Text>
+            </View>
+            <View style={{width:160, position:'absolute', left:40, top:80}}>
+              <Text style={{fontFamily:'Poppins-Bold', fontSize:14, color:'#0D986A', position:'absolute'}}>Hidupkan perangkat IoT dan tancapkan sensor ke tanah</Text>
+            </View>
+            <View style={{width:50,height:50, borderRadius:25, position:'absolute', backgroundColor:'#0D986A', right:40, top:20}}></View>
+            <View style={{width:15,height:15, borderRadius:15/2, position:'absolute', backgroundColor:'#0D986A', left:30, top:5}}></View>
+            <View style={{width:10,height:10, borderRadius:10/2, position:'absolute', backgroundColor:'#0D986A', left:23, top:30}}></View>
+            <View style={{width:30,height:30, borderRadius:30/2, position:'absolute', backgroundColor:'#0D986A', right:23, top:70}}></View>
+            <View style={{width:20,height:20, borderRadius:20/2, position:'absolute', backgroundColor:'#0D986A', left:170, bottom:10}}></View>
+            <TouchableOpacity style={{backgroundColor:'#0D986A', borderRadius:10, paddingHorizontal:20, paddingVertical:5, position:'absolute', bottom:30, right:80}}>
+              <Text style={{fontFamily:'Poppins-Bold', fontSize:18, color:'white'}}>Mulai</Text>
+            </TouchableOpacity>
+          </TouchableOpacity>
         </ScrollView>
 
         {/* Bottom Navigation */}
         <View style={{position:'absolute', bottom:0, left:0, flexDirection:'row', backgroundColor:'white', borderTopLeftRadius:20, borderTopRightRadius:20 , paddingTop:15, paddingBottom:10, justifyContent:'center', alignItems:'center'}}>
-          <TouchableOpacity style={{flex:1}}>
+          <TouchableOpacity style={{flex:1, alignItems:'center'}}>
             <Image source={iconHome} style={{height:24, width:24, resizeMode:'contain'}} />
           </TouchableOpacity>
-          <TouchableOpacity style={{flex:1}}>
+          <TouchableOpacity style={{flex:1, alignItems:'center'}}>
             <Image source={iconLove} style={{height:24, width:24, resizeMode:'contain'}} />
           </TouchableOpacity>
-          <TouchableOpacity style={{flex:1}}>
+          <TouchableOpacity style={{flex:1, alignItems:'center'}}>
             <Image source={iconBag} style={{height:24, width:24, resizeMode:'contain'}} />
           </TouchableOpacity>
-          <TouchableOpacity style={{flex:1}}>
+          <TouchableOpacity style={{flex:1, alignItems:'center'}}>
             <Image source={iconUser} style={{height:24, width:24, resizeMode:'contain'}} />
           </TouchableOpacity>
         </View>
